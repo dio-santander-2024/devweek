e329b410939ffe1e2fae15919824a0177ae5b2b9
RafaMoraes <rafaeljrmoraes@outlook.com>
2024-07-07 10:32:36 -0300

    :tada: initial commit

---
+++ src/main/java/dio/santander/desafios/devweek2024/Devweek2024Application.java
@@ -0,0 +1,13 @@
+package dio.santander.desafios.devweek2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devweek2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devweek2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/dio/santander/desafios/devweek2024/Devweek2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.santander.desafios.devweek2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devweek2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
