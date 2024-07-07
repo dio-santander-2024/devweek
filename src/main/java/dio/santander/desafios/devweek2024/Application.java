9a9f7cb7c70b57af8abcd364f4463f72c793a383
RafaMoraes <rafaeljrmoraes@outlook.com>
2024-07-07 17:16:17 -0300

    :bug: bug-fix: Corrigindo Cors

--- src/main/java/dio/santander/desafios/devweek2024/Application.java
+++ src/main/java/dio/santander/desafios/devweek2024/Application.java
@@ -1,8 +1,12 @@
 package dio.santander.desafios.devweek2024;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(servers = {@Server( url = "/api",description = "Api")})
 @SpringBootApplication
 public class Application {
 
